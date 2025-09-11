222091e7e9170faefcb2d94db37fd6619da00b43
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2025-09-10 22:55:58 -0300

    Exercício 2

--- MeuPrimeiroApp/App.js
+++ MeuPrimeiroApp/App.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Exercicio1 from './src/components/Exercicio1'
 import Exercicio2 from './src/components/Exercicio2'
 
 const App = () => {
   return (
     <View>
+      <Exercicio1 name='Nícollas'/>
       <Exercicio2/>
     </View>
   )
